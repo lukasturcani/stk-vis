db662c6b018ca9c4c4832df72ba27b5e57d8021b
Lukas Turcani <lukasturcani93@gmail.com>
2020-05-20 17:23:28 +0200

    Added optional action dispatch.

--- src/features/DatabaseBrowser/features/NextButton/components/NextButton.tsx
+++ src/features/DatabaseBrowser/features/NextButton/components/NextButton.tsx
@@ -3,21 +3,42 @@ import { connect } from 'react-redux';
 import { getNextMolecules } from '../actions';
 
 
-const _NextButton = ({onClick}) => (
-    <button onClick={onClick} >
+const _NextButton = ({moleculesRequestState, onClick}) => (
+    <button onClick={() => onClick(moleculesRequestState)} >
         Next Molecules
     </button>
 );
 
 
-const mapStateToProps = state => {
-    return {};
+const mapStateToProps = { moleculesRequestState } => {
+    return { moleculesRequestState };
 };
 
 
+const enum MoleculeRequestState {
+    NoRequestSent,
+    RequestFailure,
+    RequestSuccess,
+    RequestActive,
+};
+
+function assertNever(arg: never): never { throw Error(); }
+
 const mapDispatchToProps = dispatch => {
     return {
-        onClick: () => dispatch(getNextMolecules()),
+        onClick: (requestState: MoleculeRequestState) => {
+            switch (requestState) {
+                case MoleculeRequestState.NoRequestSent:
+                case MoleculeRequestState.RequestFailure:
+                case MoleculeRequestState.RequestSuccess:
+                    dispatch(getNextMolecules());
+                    break;
+                case MoleculeRequestState.RequestActive:
+                    break;
+                default:
+                    assertNever(requestState);
+            }
+        },
     };
 };
 
