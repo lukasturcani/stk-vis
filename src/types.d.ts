edb49a66ea77726f0e446e6c966bb9b8b0792f0f
Lukas Turcani <lukasturcani93@gmail.com>
2020-05-21 17:26:31 +0200

    Renamed.

--- src/types.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="./types/IDatabaseBrowser/IDatabaseBrowser.d.ts" />
-/// <reference path="./types/base.d.ts" />
-
-interface IFeature {
-    readonly actions: any,
-    readonly reducers: any,
-    readonly components: any,
-}
