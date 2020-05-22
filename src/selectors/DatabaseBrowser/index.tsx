ec8d6674eef77163a27994c658830425905a3265
Lukas Turcani <lukasturcani93@gmail.com>
2020-05-22 17:00:42 +0200

    Trying to get linting to work

--- src/selectors/DatabaseBrowser/index.tsx
+++ src/selectors/DatabaseBrowser/index.tsx
@@ -1,13 +1,26 @@
-import { Maybe, Just } from '../../utilities';
+import { Maybe, Just, Nothing } from '../../utilities';
 import { IState } from '../../models';
 
 
-export function getEntry({
+export function getMoleculeTableEntry({
     state,
+    columnName,
+    moleculeId,
 }: {
     state: IState,
+    columnName: string,
+    moleculeId: number,
 }): Maybe<string>
 {
-    console.log(state.databaseBrowser);
-    return new Just('');
+    const one: number = undefined;
+    const visibleColumns: IVisibleColumns = getVisibleColumns(state);
+    const entry: string | undefined
+        = visibleColumns[columnName][moleculeId];
+
+    if (entry === undefined) {
+        // This shouldn't compile because its Just<undefined>.
+        return new Just(entry);
+    } else {
+        return new Nothing();
+    }
 }
