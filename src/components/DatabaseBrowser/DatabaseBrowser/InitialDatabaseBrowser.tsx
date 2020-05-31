016f72326df4f6341d420fb6c9bf7cb5ec98471c
Lukas Turcani <lukasturcani93@gmail.com>
2020-06-01 01:16:32 +0200

    Added breacrumbs to front page.

--- src/components/DatabaseBrowser/DatabaseBrowser/InitialDatabaseBrowser.tsx
+++ src/components/DatabaseBrowser/DatabaseBrowser/InitialDatabaseBrowser.tsx
@@ -9,6 +9,17 @@ import {
 } from '../MoleculeRequestButton';
 import { MongoDbFieldsComponent } from '../MongoDbFields';
 import Container from '@material-ui/core/Container';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import Typography from '@material-ui/core/Typography';
+import Settings from '@material-ui/icons/Settings';
+import Grid from '@material-ui/core/Grid';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import {
+    createStyles,
+    makeStyles,
+    Theme,
+} from '@material-ui/core/styles';
+import { theme } from './theme';
 
 
 interface IInitialDatabaseBrowserProps
@@ -17,9 +28,43 @@ interface IInitialDatabaseBrowserProps
 }
 
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    icon: {
+      marginRight: theme.spacing(0.5),
+      width: 20,
+      height: 20,
+    },
+  }),
+);
+
+
 function InitialDatabaseBrowser(props: IInitialDatabaseBrowserProps)
 {
-    return <Container><MongoDbFieldsComponent /></Container>;
+    const classes = useStyles(theme);
+    return (
+        <Container><Grid container
+            style={{height: '100%'}}
+            spacing={3}
+        >
+            <Grid item xs={ 12 }>
+                <Breadcrumbs
+                    separator={
+                        <NavigateNextIcon className={ 'mine' } />
+                    }
+                >
+                    <Typography color='inherit' variant='body2'>
+                        <Settings className={ classes.icon } />
+                        MongoDB
+                    </Typography>
+                </Breadcrumbs>
+            </Grid>
+            <Grid item xs={ 12 }>
+                <MongoDbFieldsComponent />
+            </Grid>
+        </Grid>
+        </Container>
+    );
 }
 
 
