e6440811736ddff15b4af587c3efb293df2e0563
Lukas Turcani <lukasturcani93@gmail.com>
2020-07-13 23:26:27 +0200

    wip

---
+++ src/mongo-configurator/components/input-fields.tsx
@@ -0,0 +1,169 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import { SearchKindSelector } from './SearchKindSelector';
+
+
+interface Props
+{
+    url: string;
+    setUrl: (url: string) => void;
+
+    moleculeKey: string;
+    setMoleculeKey: (moleculeKey: string) => void;
+
+    database: string;
+    setDatabase: (database: string) => void;
+
+    moleculeCollection: string;
+    setMoleculeCollection: (collection: string) => void;
+
+    constructedMoleculeCollection: string;
+    setConstructedMoleculeCollection: (collection: string) => void;
+
+    positionMatrixCollection: string;
+    setPositionMatrixCollection: (collection: string) => void;
+
+    buildingBlockPositionMatrixCollection: string;
+    setBuildingBlockPositionMatrixCollection:
+        (collection: string) => void;
+
+    numEntriesPerPage: number;
+    setNumEntriesPerPage: (numEntries: number) => void;
+
+    selectBuildingBlocks: boolean;
+    setSelectBuildingBlocks: (select: boolean) => void;
+
+    selectConstructedMolecules: boolean;
+    setSelectConstructedMolecules: (select: boolean) => void;
+}
+
+
+export function InputFields(
+    props: Props,
+)
+{
+    const setBBPosMatCollection: (collection: string) => void
+        = props.setBuildingBlockPositionMatrixCollection;
+
+    return (
+        <Grid container>
+            <Grid item>
+                <SearchKindSelector />
+            </Grid>
+            <Grid item>
+                <TextField
+                    id='mongo-url'
+                    label='MongoDB URL'
+                    defaultValue={ props.url }
+                    variant='outlined'
+                    onChange={ (e) => props.setUrl(e.target.value) }
+                />
+            </Grid>
+            <Grid item>
+                <TextField
+                    id='mongo-molecule-key'
+                    label='Molecule Key Name'
+                    defaultValue={ props.moleculeKey }
+                    variant='outlined'
+                    onChange={
+                        (e) => props.setMoleculeKey(e.target.value)
+                    }
+                />
+            </Grid>
+            <Grid item>
+                <TextField
+                    id='mongo-database'
+                    label='Database Name'
+                    defaultValue={ props.database }
+                    variant='outlined'
+                    onChange={
+                        (e) => props.setDatabase(e.target.value)
+                    }
+                />
+            </Grid>
+            <Grid item>
+                <TextField
+                    id='mongo-molecule-collection'
+                    label='Molecule Collection Name'
+                    defaultValue={ props.moleculeCollection }
+                    variant='outlined'
+                    onChange={
+                        (e) => props.setMoleculeCollection(
+                            e.target.value
+                        )
+                    }
+                />
+            </Grid>
+            <Grid item>
+                <TextField
+                    id='mongo-constructed-molecule-collection'
+                    label='Constructed Molecule Collection Name'
+                    defaultValue={
+                        props.constructedMoleculeCollection
+                    }
+                    variant='outlined'
+                    onChange={
+                        (e) => props.setConstructedMoleculeCollection(
+                            e.target.value
+                        )
+                    }
+                />
+            </Grid>
+            <Grid item>
+                <TextField
+                    id='mongo-position-matrix-collection'
+                    label='Position Matrix Collection Name'
+                    defaultValue={ props.positionMatrixCollection }
+                    variant='outlined'
+                    onChange={
+                        (e) => props.setPositionMatrixCollection(
+                            e.target.value
+                        )
+                    }
+                />
+            </Grid>
+            {
+                props.selectBuildingBlocks
+                &&
+                props.selectConstructedMolecules
+                &&
+                <Grid item>
+                    <TextField
+                        id={
+                            'mongo-building-block-'
+                            +'position-matrix-collection'
+                        }
+                        label={
+                            'Building Block '
+                            +'Position Matrix Collection Name'
+                        }
+                        defaultValue={
+                            props.buildingBlockPositionMatrixCollection
+                        }
+                        variant='outlined'
+                        onChange={
+                            (e) => setBBPosMatCollection(
+                                e.target.value
+                            )
+                        }
+                    />
+                </Grid>
+            }
+            <Grid item>
+                <TextField
+                    id='num-entries-per-age'
+                    label='Entries Per Page'
+                    defaultValue={ props.numEntriesPerPage }
+                    variant='outlined'
+                    onChange={
+                        (e) => props.setNumEntriesPerPage(
+                            parseInt(e.target.value)
+                        )
+                    }
+                />
+            </Grid>
+        </Grid>
+    );
+}
---
+++ src/mongo-configurator/components/mongo-configurator.tsx
@@ -0,0 +1,111 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import Grid from '@material-ui/core/Grid';
+import { InputFields } from './input-fields';
+
+
+interface Props
+{
+    url: string;
+    moleculeKey: string;
+    database: string;
+    moleculeCollection: string;
+    constructedMoleculeCollection: string;
+    positionMatrixCollection: string;
+    buildingBlockPositionMatrixCollection: string;
+    numEntriesPerPage: number;
+    selectBuildingBlocks: boolean;
+    selectConstructedMolecules: boolean;
+}
+
+
+function MongoConfigurator(
+    props: Props,
+)
+{
+    const [url, setUrl]
+        = React.useState(props.url);
+
+    const [moleculeKey, setMoleculeKey]
+        = React.useState(props.moleculeKey);
+
+    const [database, setDatabase]
+        = React.useState(props.database);
+
+    const [moleculeCollection, setMoleculeCollection]
+        = React.useState(props.moleculeCollection);
+
+    const [
+        constructedMoleculeCollection,
+        setConstructedMoleculeCollection,
+    ]
+        = React.useState(props.constructedMoleculeCollection);
+
+    const [positionMatrixCollection, setPositionMatrixCollection]
+        = React.useState(props.positionMatrixCollection);
+
+    const [
+        buildingBlockPositionMatrixCollection,
+        setBuildingBlockPositionMatrixCollection,
+    ]
+        = React.useState(props.buildingBlockPositionMatrixCollection);
+
+    const [numEntriesPerPage, setNumEntriesPerPage]
+        = React.useState(props.numEntriesPerPage);
+
+    const [selectBuildingBlocks, setSelectBuildingBlocks]
+        = React.useState(props.selectBuildingBlocks);
+
+    const [selectConstructedMolecules, setSelectConstructedMolecules]
+        = React.useState(props.selectConstructedMolecules);
+
+    return (
+        <Grid container>
+            <InputFields
+                url={ url }
+                setUrl={ setUrl }
+
+                moleculeKey={ moleculeKey }
+                setMoleculeKey={ setMoleculeKey }
+
+                database={ database }
+                setDatabase={ setDatabase }
+
+                moleculeCollection={ moleculeCollection }
+                setMoleculeCollection={ setMoleculeCollection }
+
+                constructedMoleculeCollection={
+                    constructedMoleculeCollection
+                }
+                setConstructedMoleculeCollection={
+                    setConstructedMoleculeCollection
+                }
+
+                positionMatrixCollection={ positionMatrixCollection }
+                setPositionMatrixCollection={
+                    setPositionMatrixCollection
+                }
+
+                buildingBlockPositionMatrixCollection={
+                    buildingBlockPositionMatrixCollection
+                }
+                setBuildingBlockPositionMatrixCollection={
+                    setBuildingBlockPositionMatrixCollection
+                }
+
+                numEntriesPerPage={ numEntriesPerPage }
+                setNumEntriesPerPage={ setNumEntriesPerPage }
+
+                selectBuildingBlocks={ selectBuildingBlocks }
+                setSelectBuildingBlocks={ setSelectBuildingBlocks }
+
+                selectConstructedMolecules={
+                    selectConstructedMolecules
+                }
+                setSelectConstructedMolecules={
+                    setSelectConstructedMolecules
+                }
+            />
+        </Grid>
+    );
+}
