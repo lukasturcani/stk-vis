2b6cec49c6f22d4a648d8ce5bab5e66c53822569
Lukas Turcani <lukasturcani93@gmail.com>
2020-07-15 12:39:20 +0200

    added buttons

--- src/stk-vis/components/stk-vis.tsx
+++ src/stk-vis/components/stk-vis.tsx
@@ -14,7 +14,7 @@ function Component(
     {
         case 'Mongo Configurator':
         {
-            return <MongoConfigurator {...props } />;
+            return <MongoConfigurator { ...props } />;
         }
     }
 }
