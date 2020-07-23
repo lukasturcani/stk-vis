5166f23e7b2232630ca1c83ef7709eac6ffc70dd
Lukas Turcani <lukasturcani93@gmail.com>
2020-07-23 13:42:49 +0200

    Updated design

--- src/molecule-browser/MoleculeBrowser/MoleculeBrowser/Props.purs
+++ src/molecule-browser/MoleculeBrowser/MoleculeBrowser/Props.purs
@@ -10,77 +10,12 @@ import MoleculeBrowser.MoleculeBrowser.Internal.MoleculeBrowser
     ( MoleculeBrowser
     )
 
-data Props
-    = UnsortedAll
-        { kind                                  :: String
-        , url                                   :: String
-        , database                              :: String
-        , moleculeKey                           :: String
-        , moleculeCollection                    :: String
-        , positionMatrixCollection              :: String
-        , buildingBlockPositionMatrixCollection :: String
-        , numEntriesPerPage                     :: Int
-        , valueCollections                      :: String
-        , pageData                              :: PageData
-        }
-    | UnsortedOne
-        { kind                                  :: String
-        , url                                   :: String
-        , database                              :: String
-        , moleculeKey                           :: String
-        , moleculeCollection                    :: String
-        , constructedMoleculeCollection         :: String
-        , positionMatrixCollection              :: String
-        , numEntriesPerPage                     :: Int
-        , valueCollections                      :: String
-        , pageData                              :: PageData
-        }
-    | SortedAll
-        { kind                                  :: String
-        , url                                   :: String
-        , database                              :: String
-        , moleculeKey                           :: String
-        , moleculeCollection                    :: String
-        , positionMatrixCollection              :: String
-        , buildingBlockPositionMatrixCollection :: String
-        , numEntriesPerPage                     :: Int
-        , sortedCollection                      :: String
-        , sortType                              :: SortType
-        , valueCollections                      :: String
-        , pageData                              :: PageData
-        }
-    | SortedOne
-        { kind                                  :: String
-        , url                                   :: String
-        , database                              :: String
-        , moleculeKey                           :: String
-        , moleculeCollection                    :: String
-        , constructedMoleculeCollection         :: String
-        , positionMatrixCollection              :: String
-        , numEntriesPerPage                     :: Int
-        , sortedCollection                      :: String
-        , sortType                              :: SortType
-        , valueCollections                      :: String
-        , pageData                              :: PageData
-        }
 
-props :: MoleculeBrowser -> Props
-props browser
-    = UnsortedAll
-        { kind: "Unsorted All"
-        }
-    | UnsortedOne
-        { kind: "Unsorted Building Blocks"
-        }
-    | UnsortedOne
-        { kind: "Unsorted Constructed Molecules"
-        }
-    | SortedAll
-        { kind: "Sorted All"
-        }
-    | SortedOne
-        { kind: "Sorted Building Blocks"
-        }
-    | SortedOne
-        { kind: "Sorted Constructed Molecules"
-        }
+data Props = Props
+    { sortButton    :: SortButtonProps
+    , moleculeTable :: MoleculeTableProps
+    , twoDViewer    :: TwoDViewerProps
+    , threeDViewer  :: ThreeDViewerProps
+    , backButton    :: BackButtonProps
+    , nextButton    :: NextButtonProps
+    }
--- src/molecule-browser/components/base/molecule-browser.tsx
+++ src/molecule-browser/components/base/molecule-browser.tsx
@@ -1,9 +1,30 @@
 import * as React from 'react';
+import { BaseProps as SortButtonProps } from './sort-button';
+import {
+    BaseProps as MoleculeTableProps,
+} from './molecule-table';
+import {
+    BaseProps as TwoDViewerProps,
+} from './2d-viewer';
+import {
+    BaseProps as ThreeDViewerProps,
+} from './3d-viewer';
+import {
+    BaseProps as BackButtonProps,
+} from './back-button';
+import {
+    BaseProps as NextButtonProps,
+} from './next-button';
 
 
 export interface BaseProps
 {
-    kind: 'Molecule Browser';
+    sortButtonProps: SortButtonProps;
+    moleculeTableProps: MoleculeTableProps;
+    twoDViewerProps: TwoDViewerProps;
+    threeDViewerProps: ThreeDViewerProps;
+    backButtonProps: BackButtonProps;
+    nextButtonProps: NextButtonProps;
 }
 
 
@@ -14,7 +35,13 @@ export interface DispatchProps
 
 interface Props extends BaseProps, DispatchProps
 {
-    component: React.FunctionComponent<Record<string, unknown>>;
+    root: React.FunctionComponent<Record<string, unknown>>;
+    sortButton: React.FunctionComponent<SortButtonProps>;
+    moleculeTable: React.FunctionComponent<MoleculeTableProps>;
+    twoDViewer: React.FunctionComponent<TwoDViewerProps>;
+    threeDViewer: React.FunctionComponent<ThreeDViewerProps>;
+    backButton: React.FunctionComponent<BackButtonProps>;
+    nextButton: React.FunctionComponent<NextButtonProps>;
 }
 
 
@@ -22,5 +49,14 @@ export function MoleculeBrowser(
     props: Props,
 )
 {
-    return <props.component />;
+    return (
+        <props.root>
+            <props.sortButton {...props.sortButtonProps} />
+            <props.moleculeTable {...props.moleculeTableProps} />
+            <props.twoDViewer {...props.twoDViewerProps} />
+            <props.threeDViewer {...props.threeDViewerProps} />
+            <props.backButton {...props.backButtonProps} />
+            <props.nextButton {...props.nextButtonProps} />
+        </props.root>
+    );
 }
