66dd45bb999151b95fac85a6e35fbd5128d5b25d
Lukas Turcani <lukasturcani93@gmail.com>
2020-07-24 21:15:57 +0200

    added test molecule

--- src/molecules/Molecules/Molecules/Props.purs
+++ src/molecules/Molecules/Molecules/Props.purs
@@ -7,8 +7,15 @@ module Molecules.Molecules.Internal.Props
     , threeDViewerProps
     ) where
 
+import Prelude
+import Data.Either (Either (Right, Left))
 import Molecules.Molecules.Internal.Molecules (Molecules)
-import MolDraw.DrawMol.Mesh (Mesh)
+import MolDraw.DrawMol.Mesh (MeshOptions, Mesh, meshes)
+import MolDraw.Atom (atom)
+import MolDraw.Bond (bond)
+import MolDraw.ChemicalSymbol (ChemicalSymbol (..))
+import MolDraw.Position (position)
+import MolDraw.GeometryData (GeometryData, maybeMolecule)
 
 data MoleculeTableProps = MoleculeTableProps
 
@@ -28,5 +35,28 @@ data ThreeDViewerProps = ThreeDViewerProps
     { meshes :: Array Mesh
     }
 
+
+foreign import meshOptions :: MeshOptions
+
 threeDViewerProps :: Molecules -> ThreeDViewerProps
 threeDViewerProps molecules = ThreeDViewerProps
+    { meshes: meshes'
+    }
+  where
+    atoms =
+        [ atom C (position 1.0 0.0 0.0)
+        , atom H (position 2.0 0.0 0.0)
+        , atom H (position 0.0 0.0 0.0)
+        ]
+    bonds =
+        [ bond 1 0 1
+        , bond 1 0 2
+        ]
+    molecule = maybeMolecule atoms bonds
+    meshes' = do
+        geometryData <- maybeToArray molecule
+        meshes meshOptions geometryData
+
+maybeToArray :: Either String GeometryData -> Array GeometryData
+maybeToArray (Left _) = []
+maybeToArray (Right geometryData) = [geometryData]
