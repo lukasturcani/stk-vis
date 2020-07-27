aeabae3460f412f7e312e5e0499c599d11ce0c14
Lukas Turcani <lukasturcani93@gmail.com>
2020-07-27 03:24:16 +0200

    wip

--- src/stk-vis/actions/InitializeMoleculeBrowser.purs
+++ src/stk-vis/actions/InitializeMoleculeBrowser.purs
@@ -1,10 +1,17 @@
 module StkVis.InitializeMoleculeBrowser
     ( InitializeMoleculeBrowser
     , toMoleculeBrowser
+    , initializeMoleculeBrowser
     ) where
+
+import Prelude
+import RequestManager.RequestManager (RequestManager)
+import SelectingCollection (SelectingCollection)
+import Molecules.Molecule (Molecule)
 
 import MoleculeBrowser.InitializeMoleculeBrowser
     ( InitializeMoleculeBrowser
+    , initializeMoleculeBrowser
     ) as MoleculeBrowser
 
 newtype InitializeMoleculeBrowser
@@ -15,3 +22,16 @@ toMoleculeBrowser
     :: InitializeMoleculeBrowser
     -> MoleculeBrowser.InitializeMoleculeBrowser
 toMoleculeBrowser (InitializeMoleculeBrowser payload) = payload
+
+initializeMoleculeBrowser
+    :: RequestManager
+    -> SelectingCollection Molecule
+    -> Array String
+    -> InitializeMoleculeBrowser
+initializeMoleculeBrowser requestManager molecules columns
+    = InitializeMoleculeBrowser $
+        MoleculeBrowser.initializeMoleculeBrowser
+            requestManager
+            molecules
+            columns
+
