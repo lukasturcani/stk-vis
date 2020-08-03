1d0289715c54f3ec18b929374c6deeb2d955f719
Lukas Turcani <lukasturcani93@gmail.com>
2020-08-03 11:40:39 +0200

    updated types

--- src/@types/Molecules.Action.d.ts
+++ src/@types/Molecules.Action.d.ts
@@ -1,8 +1,13 @@
 declare module 'Molecules.Action'
 {
+    import { SelectMolecule } from 'Molecules.SelectMolecule';
+
     export interface Action
     {
         type: string;
     }
+
+    export const selectMolecule:
+        (select: SelectMolecule) => Action;
 
 }
