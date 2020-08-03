969ab1d1865f01da6e8bfab5c8e3437692035e16
Lukas Turcani <lukasturcani93@gmail.com>
2020-08-03 13:19:28 +0200

    added content

--- src/request-manager/RequestManager/RequestManager/Internal.purs
+++ src/request-manager/RequestManager/RequestManager/Internal.purs
@@ -2,4 +2,94 @@ module RequestManager.RequestManager.Internal.RequestManager
     ( RequestManager (..)
     ) where
 
-data RequestManager = RequestManager
+data RequestManager
+    = UnsortedAll UnsortedAll
+    | UnsortedBuildingBlocks UnsortedBuildingBlocks
+    | UnsortedConstructedMolecules UnsortedConstructedMolecules
+    | SortedAll SortedAll
+    | SortedBuildingBlocks SortedBuildingBlocks
+    | SortedConstructedMolecules SortedConstructedMolecules
+
+data UnsortedAll = UnsortedAll
+    { _url                                   :: String
+    , _database                              :: String
+    , _moleculeKey                           :: String
+    , _moleculeCollection                    :: String
+    , _positionMatrixCollection              :: String
+    , _buildingBlockPositionMatrixCollection :: String
+    , _pageIndex                             :: Int
+    , _numEntriesPerPage                     :: Int
+    , _ignoredCollections                    :: Array String
+    , _pageKind                              :: PageKind
+    }
+
+data UnsortedBuildingBlocks
+    { _url                                   :: String
+    , _database                              :: String
+    , _moleculeKey                           :: String
+    , _moleculeCollection                    :: String
+    , _constructedMoleculeCollection         :: String
+    , _positionMatrixCollection              :: String
+    , _pageIndex                             :: Int
+    , _numEntriesPerPage                     :: Int
+    , _ignoredCollections                    :: Array String
+    , _pageKind                              :: PageKind
+    }
+
+data UnsortedConstructedMolecules
+    { _url                                   :: String
+    , _database                              :: String
+    , _moleculeKey                           :: String
+    , _moleculeCollection                    :: String
+    , _constructedMoleculeCollection         :: String
+    , _positionMatrixCollection              :: String
+    , _pageIndex                             :: Int
+    , _numEntriesPerPage                     :: Int
+    , _ignoredCollections                    :: Array String
+    , _pageKind                              :: PageKind
+    }
+
+data SortedAll = SortedAll
+    { _url                                   :: String
+    , _database                              :: String
+    , _moleculeKey                           :: String
+    , _moleculeCollection                    :: String
+    , _positionMatrixCollection              :: String
+    , _buildingBlockPositionMatrixCollection :: String
+    , _pageIndex                             :: Int
+    , _numEntriesPerPage                     :: Int
+    , _ignoredCollections                    :: Array String
+    , _sortedCollection                      :: String
+    , _sortType                              :: SortType
+    , _pageKind                              :: PageKind
+    }
+
+data SortedBuildingBlocks = SortedBuildingBlocks
+    { _url                                   :: String
+    , _database                              :: String
+    , _moleculeKey                           :: String
+    , _moleculeCollection                    :: String
+    , _constructedMoleculeCollection         :: String
+    , _positionMatrixCollection              :: String
+    , _pageIndex                             :: Int
+    , _numEntriesPerPage                     :: Int
+    , _ignoredCollections                    :: Array String
+    , _sortedCollection                      :: String
+    , _sortType                              :: SortType
+    , _pageKind                              :: PageKind
+    }
+
+data SortedConstructedMolecules = SortedConstructedMolecules
+    { _url                                   :: String
+    , _database                              :: String
+    , _moleculeKey                           :: String
+    , _moleculeCollection                    :: String
+    , _constructedMoleculeCollection         :: String
+    , _positionMatrixCollection              :: String
+    , _pageIndex                             :: Int
+    , _numEntriesPerPage                     :: Int
+    , _ignoredCollections                    :: Array String
+    , _sortedCollection                      :: String
+    , _sortType                              :: SortType
+    , _pageKind                              :: PageKind
+    }
