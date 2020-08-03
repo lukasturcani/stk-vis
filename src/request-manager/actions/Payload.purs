1e36d090cc10bcf82737beb1562e36113545ac4e
Lukas Turcani <lukasturcani93@gmail.com>
2020-08-03 15:39:38 +0200

    wip

--- src/request-manager/actions/Payload.purs
+++ src/request-manager/actions/Payload.purs
@@ -1,14 +1,25 @@
 module RequestManager.Payload
     ( Payload (..)
     , initializeUnsortedAll
+    , initializeUnsortedBuildingBlocks
     ) where
 
 import RequestManager.InitializeUnsortedAll
     ( InitializeUnsortedAll
     )
 
+import RequestManager.InitializeUnsortedBuildingBlocks
+    ( InitializeUnsortedBuildingBlocks
+    )
+
 data Payload
     = InitializeUnsortedAll InitializeUnsortedAll
+    | InitializeUnsortedBuildingBlocks InitializeUnsortedBuildingBlocks
 
 initializeUnsortedAll :: InitializeUnsortedAll -> Payload
 initializeUnsortedAll = InitializeUnsortedAll
+
+initializeUnsortedBuildingBlocks
+    :: InitializeUnsortedBuildingBlocks -> Payload
+
+initializeUnsortedBuildingBlocks = InitializeUnsortedBuildingBlocks
