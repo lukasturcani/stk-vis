dd32b6184481ff98c4317095adeb916836542e41
Lukas Turcani <lukasturcani93@gmail.com>
2020-08-04 06:01:53 +0200

    updated props

--- src/request-manager/RequestManager/RequestManager/Props/NextButton.purs
+++ src/request-manager/RequestManager/RequestManager/Props/NextButton.purs
@@ -4,9 +4,24 @@ module RequestManager.RequestManager.Internal.Props.Internal.NextButton
     ) where
 
 import RequestManager.RequestManager.Internal.RequestManager
-    (RequestManager)
+    ( RequestManager
+    , _pageKind
+    )
+
+import RequestManager.PageKind (PageKind (..))
 
 data NextButtonProps = NextButtonProps
+    { lastPage :: Boolean
+    }
 
 nextButtonProps :: RequestManager -> NextButtonProps
-nextButtonProps requestManager = NextButtonProps
+nextButtonProps manager = NextButtonProps
+    { lastPage: _lastPage (_pageKind manager)
+    }
+
+_lastPage :: PageKind -> Boolean
+_lastPage LastComplete   = true
+_lastPage LastIncomplete = true
+_lastPage OnlyComplete   = true
+_lastPage OnlyIncomplete = true
+_lastPage _              = false
