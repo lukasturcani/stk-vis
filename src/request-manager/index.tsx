323eb6b7c4d0f7c4a8ed3ec162131518d723a812
Lukas Turcani <lukasturcani93@gmail.com>
2020-08-04 03:07:51 +0200

    wip

--- src/request-manager/index.tsx
+++ src/request-manager/index.tsx
@@ -14,7 +14,23 @@ import {
     initialState,
     RequestManager,
 } from 'RequestManager.RequestManager';
-import { Action } from 'RequestManager.Action';
+import {
+    Action,
+    initializeUnsortedAll,
+    initializeUnsortedBuildingBlocks,
+    initializeUnsortedConstructedMolecules,
+    initializeSortedAll,
+    initializeSortedBuildingBlocks,
+    initializeSortedConstructedMolecules,
+} from 'RequestManager.Action';
+import {
+    PageKind,
+    Middle,
+} from 'RequestManager.PageKind';
+import {
+    SortType,
+    ascending,
+} from 'RequestManager.SortType';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
@@ -36,6 +52,45 @@ const store = createStore(
     applyMiddleware(logger, thunk),
 );
 
+const url: string
+    = 'mongodb://localhost:27017';
+
+const database: string
+    = 'stkVis';
+
+const moleculeKey: string
+    = 'InChIKey';
+
+const moleculeCollection: string
+    = 'molecules';
+
+const constructedMoleculeCollection: string
+    = 'constructed_molecules';
+
+const positionMatrixCollection: string
+    = 'position_matrices';
+
+const buildingBlockPositionMatrixCollection: string
+    = 'building_block_position_matrices';
+
+const pageIndex: number
+    = 0;
+
+const numEntriesPerPage: number
+    = 34;
+
+const ignoredCollections: string[]
+    = [];
+
+const pageKind: PageKind
+    = Middle;
+
+const sortedCollection: string
+    = 'numAtoms';
+
+const sortType: SortType
+    = ascending;
+
 
 ReactDOM.render(
     <Provider store={ store }>
@@ -45,6 +100,139 @@ ReactDOM.render(
                 <BackButton />
                 <NextButton />
                 <SortButton />
+
+                <button
+                    onClick={
+                        () => store.dispatch(
+                            initializeUnsortedAll({
+                                url,
+                                database,
+                                moleculeKey,
+                                moleculeCollection,
+                                positionMatrixCollection,
+                                buildingBlockPositionMatrixCollection,
+                                pageIndex,
+                                numEntriesPerPage,
+                                ignoredCollections,
+                                pageKind,
+                            })
+                        )
+                    }
+                >
+                    UnsortedAll
+                </button>
+
+                <button
+                    onClick={
+                        () => store.dispatch(
+                            initializeUnsortedBuildingBlocks({
+                                url,
+                                database,
+                                moleculeKey,
+                                moleculeCollection,
+                                constructedMoleculeCollection,
+                                positionMatrixCollection,
+                                pageIndex,
+                                numEntriesPerPage,
+                                ignoredCollections,
+                                pageKind,
+                            })
+                        )
+                    }
+                >
+                    UnsortedBuildingBlocks
+                </button>
+
+                <button
+                    onClick={
+                        () => store.dispatch(
+                            initializeUnsortedConstructedMolecules({
+                                url,
+                                database,
+                                moleculeKey,
+                                moleculeCollection,
+                                constructedMoleculeCollection,
+                                positionMatrixCollection,
+                                pageIndex,
+                                numEntriesPerPage,
+                                ignoredCollections,
+                                pageKind,
+                            })
+                        )
+                    }
+                >
+                    UnsortedConstructedMolecules
+                </button>
+
+                <button
+                    onClick={
+                        () => store.dispatch(
+                            initializeSortedAll({
+                                url,
+                                database,
+                                moleculeKey,
+                                moleculeCollection,
+                                positionMatrixCollection,
+                                buildingBlockPositionMatrixCollection,
+                                pageIndex,
+                                numEntriesPerPage,
+                                ignoredCollections,
+                                pageKind,
+                                sortedCollection,
+                                sortType,
+                            })
+                        )
+                    }
+                >
+                    SortedAll
+                </button>
+
+                <button
+                    onClick={
+                        () => store.dispatch(
+                            initializeSortedBuildingBlocks({
+                                url,
+                                database,
+                                moleculeKey,
+                                moleculeCollection,
+                                constructedMoleculeCollection,
+                                positionMatrixCollection,
+                                pageIndex,
+                                numEntriesPerPage,
+                                ignoredCollections,
+                                pageKind,
+                                sortedCollection,
+                                sortType,
+                            })
+                        )
+                    }
+                >
+                    SortedBuildingBlocks
+                </button>
+
+                <button
+                    onClick={
+                        () => store.dispatch(
+                            initializeSortedConstructedMolecules({
+                                url,
+                                database,
+                                moleculeKey,
+                                moleculeCollection,
+                                constructedMoleculeCollection,
+                                positionMatrixCollection,
+                                pageIndex,
+                                numEntriesPerPage,
+                                ignoredCollections,
+                                pageKind,
+                                sortedCollection,
+                                sortType,
+                            })
+                        )
+                    }
+                >
+                    SortedConstructedMolecules
+                </button>
+
             </div>
         </ThemeProvider>
     </Provider>,
