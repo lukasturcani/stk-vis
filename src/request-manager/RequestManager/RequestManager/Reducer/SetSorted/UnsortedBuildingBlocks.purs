fb5529b35f1ccfa76c70625cd3453abeba15f100
Lukas Turcani <lukasturcani93@gmail.com>
2020-08-05 11:35:45 +0200

    wip

---
+++ src/request-manager/RequestManager/RequestManager/Reducer/SetSorted/UnsortedBuildingBlocks.purs
@@ -0,0 +1,51 @@
+module RequestManager.RequestManager.Internal.Reducer.Internal.SetSorted.Internal.UnsortedBuildingBlocks
+    ( setSorted
+    ) where
+
+import RequestManager.RequestManager.Internal.RequestManager
+    as RequestManager
+
+import RequestManager.SetSorted as SetSorted
+
+import RequestManager.RequestManager.Internal.RequestManager.SortedBuildingBlocks
+    as SortedBuildingBlocks
+
+import RequestManager.RequestManager.Internal.RequestManager.UnsortedBuildingBlocks
+    as UnsortedBuildingBlocks
+
+setSorted
+    :: UnsortedBuildingBlocks.UnsortedBuildingBlocks
+    -> SetSorted.SetSorted
+    -> RequestManager.RequestManager
+
+setSorted
+    (UnsortedBuildingBlocks.UnsortedBuildingBlocks
+        { _url
+        , _database
+        , _moleculeKey
+        , _moleculeCollection
+        , _positionMatrixCollection
+        , _buildingBlockPositionMatrixCollection
+        , _pageIndex
+        , _numEntriesPerPage
+        , _ignoredCollections
+        , _pageKind
+        }
+    )
+    payload
+    = RequestManager.SortedBuildingBlocks
+        (SortedBuildingBlocks.SortedBuildingBlocks
+            { _url
+            , _database
+            , _moleculeKey
+            , _moleculeCollection
+            , _positionMatrixCollection
+            , _buildingBlockPositionMatrixCollection
+            , _pageIndex
+            , _numEntriesPerPage
+            , _ignoredCollections
+            , _pageKind
+            , _sortedCollection: SetSorted.collection payload
+            , _sortType: SetSorted.sortType payload
+            }
+        )
---
+++ src/request-manager/RequestManager/RequestManager/Reducer/SetSorted/UnsortedConstructedMolecules.purs
@@ -0,0 +1,51 @@
+module RequestManager.RequestManager.Internal.Reducer.Internal.SetSorted.Internal.UnsortedConstructedMolecules
+    ( setSorted
+    ) where
+
+import RequestManager.RequestManager.Internal.RequestManager
+    as RequestManager
+
+import RequestManager.SetSorted as SetSorted
+
+import RequestManager.RequestManager.Internal.RequestManager.SortedConstructedMolecules
+    as SortedConstructedMolecules
+
+import RequestManager.RequestManager.Internal.RequestManager.UnsortedConstructedMolecules
+    as UnsortedConstructedMolecules
+
+setSorted
+    :: UnsortedConstructedMolecules.UnsortedConstructedMolecules
+    -> SetSorted.SetSorted
+    -> RequestManager.RequestManager
+
+setSorted
+    (UnsortedConstructedMolecules.UnsortedConstructedMolecules
+        { _url
+        , _database
+        , _moleculeKey
+        , _moleculeCollection
+        , _positionMatrixCollection
+        , _buildingBlockPositionMatrixCollection
+        , _pageIndex
+        , _numEntriesPerPage
+        , _ignoredCollections
+        , _pageKind
+        }
+    )
+    payload
+    = RequestManager.SortedConstructedMolecules
+        (SortedConstructedMolecules.SortedConstructedMolecules
+            { _url
+            , _database
+            , _moleculeKey
+            , _moleculeCollection
+            , _positionMatrixCollection
+            , _buildingBlockPositionMatrixCollection
+            , _pageIndex
+            , _numEntriesPerPage
+            , _ignoredCollections
+            , _pageKind
+            , _sortedCollection: SetSorted.collection payload
+            , _sortType: SetSorted.sortType payload
+            }
+        )
