50d1b83d3292499d053676828c29c0c088557937
Lukas Turcani <lukasturcani93@gmail.com>
2020-08-05 09:51:18 +0200

    Added actions.

---
+++ src/request-manager/actions/SetSorted.purs
@@ -0,0 +1,27 @@
+module RequestManager.SetSorted
+    ( SetSorted
+    , setSorted
+    , collection
+    , sortType
+    ) where
+
+import RequestManager.SortType (SortType)
+
+type CollectionName = String
+
+data SetSorted = SetSorted
+    { _collection :: CollectionName
+    , _sortType   :: SortType
+    }
+
+setSorted :: CollectionName -> SortType -> SetSorted
+setSorted name sortType' = SetSorted
+    { _collection: name
+    , _sortType: sortType'
+    }
+
+collection :: SetSorted -> CollectionName
+collection (SetSorted { _collection }) = _collection
+
+sortType :: SetSorted -> SortType
+sortType (SetSorted { _sortType }) = _sortType
---
+++ src/request-manager/actions/SetUnsorted.purs
@@ -0,0 +1,8 @@
+module RequestManager.SetUnsorted
+    ( SetUnsorted
+    ) where
+
+data SetUnsorted = SetUnsorted
+
+setUnsorted :: SetUnsorted
+setUnsorted = SetUnsorted
