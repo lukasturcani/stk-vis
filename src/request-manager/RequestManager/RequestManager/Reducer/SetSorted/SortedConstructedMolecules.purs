313c67000b367017d1df6e7f0433cf834bfdad29
Lukas Turcani <lukasturcani93@gmail.com>
2020-08-05 11:28:41 +0200

    wip

---
+++ src/request-manager/RequestManager/RequestManager/Reducer/SetSorted/SortedConstructedMolecules.purs
@@ -0,0 +1,48 @@
+module RequestManager.RequestManager.Internal.Reducer.Internal.SetSorted.Internal.SortedConstructedMolecules
+    ( setSorted
+    ) where
+
+import RequestManager.RequestManager.Internal.RequestManager
+    as RequestManager
+
+import RequestManager.SetSorted as SetSorted
+
+import RequestManager.RequestManager.Internal.RequestManager.SortedConstructedMolecules
+    as SortedConstructedMolecules
+
+setSorted
+    :: SortedConstructedMolecules
+    -> SetSorted.SetSorted
+    -> RequestManager.RequestManager
+
+setSorted
+    (SortedConstructedMolecules.SortedConstructedMolecules
+        { _url
+        , _database
+        , _moleculeKey
+        , _moleculeCollection
+        , _constructedMoleculeCollection
+        , _positionMatrixCollection
+        , _pageIndex
+        , _numEntriesPerPage
+        , _ignoredCollections
+        , _pageKind
+        }
+    )
+    payload
+    = RequestManager.SortedConstructedMolecules
+        (SortedConstructedMolecules.SortedConstructedMolecules
+            { _url
+            , _database
+            , _moleculeKey
+            , _moleculeCollection
+            , _constructedMoleculeCollection
+            , _positionMatrixCollection
+            , _pageIndex
+            , _numEntriesPerPage
+            , _ignoredCollections
+            , _pageKind
+            , _sortedCollection: SetSorted.collection payload
+            , _sortType: SetSorted.sortType payload
+            }
+        )
