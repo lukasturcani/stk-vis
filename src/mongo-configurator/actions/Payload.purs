588f2c64c106791b72bce8b3e9909300be84fb7f
Lukas Turcani <lukasturcani93@gmail.com>
2020-08-08 12:44:58 +0200

    wip

--- src/mongo-configurator/actions/Payload.purs
+++ src/mongo-configurator/actions/Payload.purs
@@ -1,60 +1,12 @@
 module MongoConfigurator.Payload
     ( Payload (..)
     , updateFields
-    , initializeSortedAll
-    , initializeSortedBuildingBlocks
-    , initializeSortedConstructedMolecules
-    , initializeUnsortedAll
-    , initializeUnsortedBuildingBlocks
-    , initializeUnsortedConstructedMolecules
     ) where
 
 import MongoConfigurator.UpdateFields.UpdateFields (UpdateFields)
 
 data Payload
     = UpdateFields UpdateFields
-    | InitializeSortedAll InitializeUnsortedAll
-    | InitializeSortedBuildingBlocks InitializeSortedBuildingBlocks
-    | InitializeSortedConstructedMolecules
-        InitializeSortedConstructedMolecules
-    | InitializeUnsortedAll InitializeUnsortedAll
-    | InitializeUnsortedBuildingBlocks InitializeUnsortedBuildingBlocks
-    | InitializeUnsortedConstructedMolecules
-        InitializeUnsortedConstructedMolecules
 
 updateFields :: UpdateFields -> Payload
 updateFields = UpdateFields
-
-initializeSortedAll :: InitializeSortedAll -> Payload
-initializeSortedAll = InitializeSortedAll
-
-initializeSortedBuildingBlocks
-    :: InitializeSortedBuildingBlocks
-    -> Payload
-
-initializeSortedBuildingBlocks = InitializeSortedBuildingBlocks
-
-initializeSortedConstructedMolecules
-    :: InitializeSortedConstructedMolecules
-    -> Payload
-
-initializeSortedConstructedMolecules
-    = InitializeSortedConstructedMolecules
-
-initializeUnsortedAll :: InitializeUnsortedAll -> Payload
-initializeUnsortedAll = InitializeUnsortedAll
-
-initializeUnsortedBuildingBlocks
-    :: InitializeUnsortedBuildingBlocks
-    -> Payload
-
-initializeUnsortedBuildingBlocks
-    = InitializeUnsortedBuildingBlocks
-
-initializeUnsortedConstructedMolecules
-    :: InitializeUnsortedConstructedMolecules
-    -> Payload
-
-initializeUnsortedConstructedMolecules
-    = InitializeUnsortedConstructedMolecules
-
