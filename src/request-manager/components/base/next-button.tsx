fe1af107921c237d2bd931f1145255d31fc24107
Lukas Turcani <lukasturcani93@gmail.com>
2020-08-11 10:15:27 +0200

    basic outline

--- src/request-manager/components/base/next-button.tsx
+++ src/request-manager/components/base/next-button.tsx
@@ -10,9 +10,14 @@ export interface DispatchProps<a>
 
 export type CoreProps<a> = DispatchProps<a> & NextButtonProps<a>;
 
+type Empty = Record<string, unknown>;
+
 interface Props<a> extends NextButtonProps<a>, DispatchProps<a>
 {
+    container: React.FunctionComponent<Empty>;
     button: React.FunctionComponent<ButtonProps>;
+    successSnackbar: React.FunctionComponent<SnackbarProps>;
+    errorSnackbar: React.FunctionComponent<SnackbarProps>;
 }
 
 
@@ -21,12 +26,55 @@ export interface ButtonProps
     onClick: () => void;
 }
 
+export interface SnackbarProps
+{
+    open: boolean;
+}
+
+
+interface Snackbar
+{
+    open: boolean;
+    setOpen: (open: boolean) => void;
+    message: string;
+    setMessage: (message: string) => void;
+}
+
 
 export function NextButton<a>(
     props: Props<a>,
 )
 {
-    return <props.button
-        onClick={() => props.value0.onClick()(props.dispatch)}
-    />;
+    const successSnackbar: Snackbar
+        = getSnackbar();
+
+    const errorSnackbar: Snackbar
+        = getSnackbar();
+
+    return (
+        <props.container>
+            <props.button
+                onClick={() => props.value0.onClick()(props.dispatch)}
+            />
+            <props.successSnackbar
+                open={successSnackbar.open}
+            />
+            <props.errorSnackbar
+                open={errorSnackbar.open}
+            />
+        </props.container>
+    );
+}
+
+
+function getSnackbar(): Snackbar
+{
+    const [open, setOpen] = React.useState(false);
+    const [message, setMessage] = React.useState('');
+    return {
+        open,
+        setOpen,
+        message,
+        setMessage,
+    };
 }
