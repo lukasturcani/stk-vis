2ac5b0873943fb0c95d245feab6da55a668ce107
Lukas Turcani <lukasturcani93@gmail.com>
2020-08-11 10:49:03 +0200

    wip

--- src/request-manager/RequestManager/RequestManager/Props/NextButton/Props.purs
+++ src/request-manager/RequestManager/RequestManager/Props/NextButton/Props.purs
@@ -1,14 +1,32 @@
 module RequestManager.RequestManager.Internal.Props.Internal.NextButton.Internal.Props
     ( NextButtonProps (..)
+    , DispatchAction
+    , Snackbar
+    , Snackbars
     ) where
 
 import Prelude
 import Effect.Promise (class Deferred, Promise)
 import Effect (Effect)
 
+type DispatchAction a = a -> Effect Unit
+
+type Snackbar =
+    { setOpen    :: Boolean -> Effect Unit
+    , setMessage :: String -> Effect Unit
+    }
+
+type Snackbars =
+    { success :: Snackbar
+    , error   :: Snackbar
+    }
+
 data NextButtonProps a = NextButtonProps
     { lastPage :: Boolean
 
     , onClick
-        :: Deferred => (a -> Effect Unit) -> Promise (Effect Unit)
+        :: Deferred
+        => DispatchAction a
+        -> Snackbars
+        -> Promise (Effect Unit)
     }
