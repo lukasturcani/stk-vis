9ad4066b258c23e4c4a107b35548dda49daf5b38
Lukas Turcani <lukasturcani93@gmail.com>
2020-08-20 10:22:55 +0200

    Added props

--- src/Page/MoleculeBrowser/Props/Props.purs
+++ src/Page/MoleculeBrowser/Props/Props.purs
@@ -1,5 +1,5 @@
 module Page.MoleculeBrowser.Props
-    ( Props
+    ( Props (..)
     ) where
 
 import Page.MoleculeBrowser.SortButton as SortButton
@@ -10,7 +10,43 @@ import Page.MoleculeBrowser.NextButton as NextButton
 import Page.MoleculeBrowser.BackButton as BackButton
 import Page.MoleculeBrowser.Breadcrumbs as Breadcrumbs
 
-type Props a =
+data Props a
+    = NoViewer NoViewer a
+    | TwoDViewer TwoDViewer a
+    | ThreeDViewer ThreeDViewer a
+    | AllViewers AllViewers a
+
+
+type NoViewer a =
+    { sortButton    :: SortButton.Props a
+    , moleculeTable :: MoleculeTable.Props a
+    , nextButton    :: NextButton.Props a
+    , backButton    :: BackButton.Props a
+    , breadcrumbs   :: Breadcrumbs.Props a
+    , type          :: String
+    }
+
+type TwoDViewer a =
+    { sortButton    :: SortButton.Props a
+    , moleculeTable :: MoleculeTable.Props a
+    , twoDViewer    :: TwoDViewer.Props
+    , nextButton    :: NextButton.Props a
+    , backButton    :: BackButton.Props a
+    , breadcrumbs   :: Breadcrumbs.Props a
+    , type          :: String
+    }
+
+type ThreeDViewer a =
+    { sortButton    :: SortButton.Props a
+    , moleculeTable :: MoleculeTable.Props a
+    , threeDViewer  :: ThreeDViewer.Props
+    , nextButton    :: NextButton.Props a
+    , backButton    :: BackButton.Props a
+    , breadcrumbs   :: Breadcrumbs.Props a
+    , type          :: String
+    }
+
+type AllViewers a =
     { sortButton    :: SortButton.Props a
     , moleculeTable :: MoleculeTable.Props a
     , twoDViewer    :: TwoDViewer.Props
