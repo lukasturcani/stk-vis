60002106eeab975f4994c1c5236dd68ad0c159c9
Lukas Turcani <lukasturcani93@gmail.com>
2021-05-31 19:27:26 +0100

    wip

--- src/Requests/Utils/Utils.purs
+++ src/Requests/Utils/Utils.purs
@@ -2,33 +2,21 @@ module Requests.Utils
     ( ConstructedMoleculeCollectionName
     , dataQuery
     , moleculeQuery
-    , addPositionMatrices
     , addValues
     ) where
 
 import Prelude
-import Data.Array (zipWith, concatMap)
-import Data.Map (Map, toUnfoldable, lookup, fromFoldable)
+import Data.Array as Array
+import Data.Map (Map)
 import Data.Map as Map
 import Data.Tuple (Tuple (Tuple))
 import Data.Maybe (Maybe (Just, Nothing))
-import Data.Maybe.Utils as Maybe
 import Mongo as Mongo
 import Requests.MoleculeKey (MoleculeKeyName, MoleculeKeyValue)
-import Requests.PositionMatrix (PositionMatrix, matrix)
-import ValidatedMolecule.Position as Position
-import ValidatedMolecule as Validated
+import Requests.Collection (Collection)
 import Requests.Collection as Collection
+import Requests.Molecule (Molecule)
 import Requests.Molecule as Molecule
-
-import Requests.Molecule
-    ( Molecule
-    , toValidated
-    , key
-    , properties
-    , constructed
-    , fromValidated
-    )
 
 foreign import dataQuery
     :: MoleculeKeyName
@@ -43,53 +31,10 @@ foreign import moleculeQuery
     -> Array MoleculeKeyValue
     -> Mongo.AggregationQuery
 
-
-addPositionMatrices
-    :: Map MoleculeKeyValue Molecule
-    -> Map MoleculeKeyValue PositionMatrix
-    -> Array Molecule
-
-addPositionMatrices molecules matrices = do
-    (Tuple key molecule) <- toUnfoldable molecules
-    positionMatrix <- Maybe.toArray (lookup key matrices)
-    Maybe.toArray (molecule `addPositionMatrix` positionMatrix)
-
-addPositionMatrix :: Molecule -> PositionMatrix -> Maybe Molecule
-addPositionMatrix molecule positionMatrix = do
-    newMolecule <- Validated.molecule atoms bonds
-    pure $ fromValidated
-        (constructed molecule)
-        (key molecule)
-        (properties molecule)
-        newMolecule
-  where
-    validated = toValidated molecule
-    atoms =
-        zipWith
-        setPosition
-        (Validated.atoms validated)
-        (matrix positionMatrix)
-    bonds = map toBond (Validated.bonds validated)
-
-    setPosition
-        :: Validated.MoleculeAtom
-        -> Position.Position
-        -> Validated.Atom
-
-    setPosition atom position =
-        Validated.atom (Validated.chemicalSymbol atom) position
-
-    toBond :: Validated.MoleculeBond -> Validated.Bond
-    toBond bond = Validated.bond order atom1Id atom2Id
-      where
-        order = Validated.order bond
-        atom1Id = Validated.id $ Validated.atom1 bond
-        atom2Id = Validated.id $ Validated.atom2 bond
-
 addValues
-    :: Array Molecule.Molecule
-    -> Array Collection.Collection
-    -> Array Molecule.Molecule
+    :: Array Molecule
+    -> Array Collection
+    -> Array Molecule
 
 addValues molecules collections = do
     molecule <- molecules
@@ -105,20 +50,21 @@ addValues molecules collections = do
 
     pure $
         Molecule.fromValidated
-            (constructed molecule)
-            (key molecule)
+            (Molecule.constructed molecule)
+            (Molecule.key molecule)
             properties
-            (toValidated molecule)
+            (Molecule.toValidated molecule)
 
   where
 
     getCollectionProperties
-        :: Molecule.Molecule
-        -> Array Collection.Collection
+        :: Molecule
+        -> Array Collection
         -> Map String String
 
     getCollectionProperties molecule collections' =
-        fromFoldable $ concatMap (value (key molecule)) collections'
+        Map.fromFoldable $
+        Array.concatMap (value (Molecule.key molecule)) collections'
 
     value key collection = case Collection.get collection key of
         Just x -> [Tuple (Collection.name collection) x]
