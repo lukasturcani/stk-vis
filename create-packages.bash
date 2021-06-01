053224ef1924e7b8a729050379792b423f853878
Lukas Turcani <lukasturcani93@gmail.com>
2021-06-01 18:56:52 +0100

    Fix arg

--- create-packages.bash
+++ create-packages.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 rm -r pack
-electron-packager dist stk-vis --all --out pack
+yarn electron-packager dist stk-vis --all --out pack
 cd pack
 mv stk-vis-darwin-x64 stk-vis-apple-darwin-x64
 for FOLDER in *
