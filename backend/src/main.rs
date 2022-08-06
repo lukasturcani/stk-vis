af5834f265e52fff7429499cf22ae0df4b22a964
Lukas Turcani <lukasturcani@mailbox.org>
2022-08-06 22:15:35 +0100

    wip

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,29 +1,14 @@
-use mongodb::{bson::doc, sync::Client};
-use serde::{Deserialize, Serialize};
+#![feature(proc_macro_hygiene, decl_macro)]
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Atom(u8, i8);
+#[macro_use]
+extern crate rocket;
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Periodicity(i8, i8, i8);
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Bond(i32, i32, f32, Periodicity);
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Molecule {
-    a: Vec<Atom>,
-    b: Vec<Bond>,
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
 }
 
-fn main() -> mongodb::error::Result<()> {
-    let client = Client::with_uri_str("mongodb://localhost:27017/")?;
-    let database = client.database("_stk_pytest_database");
-    let collection = database.collection::<Molecule>("molecules");
-    let mut cursor = collection.find(doc! {}, None)?;
-    for _ in 0..2 {
-        println!("{:#?}", cursor.deserialize_current());
-        cursor.advance()?;
-    }
-    Ok(())
+fn main() {
+    rocket::ignite().mount("/", routes![index]).launch();
 }
+
