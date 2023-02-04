a23d750c8ef47e0b34caa08ebea60b5a4f6f5a0f
Lukas Turcani <lukasturcani@mailbox.org>
2023-02-04 18:03:34 +0000

    Add interfaces

--- typescript/src/TwoDMoleculeViewer.ts
+++ typescript/src/TwoDMoleculeViewer.ts
@@ -1,10 +1,29 @@
-import * as SmilesDrawer from 'smiles-drawer';
-class TwoDMoleculeViewer extends HTMLElement {
+// import * as SmilesDrawer from 'smiles-drawer';
+
+interface Atom {
+  atomicNumber: number;
+}
+
+interface Bond {
+  order: number;
+  atom1: number;
+  atom2: number;
+}
+
+interface Molecule {
+  atoms: Atom[];
+  bonds: Bond[];
+}
+
+export class TwoDMoleculeViewer extends HTMLElement {
+  _smiles?: string;
+
   constructor() {
     super();
+
   }
 
-  get molecule(): {
+  set molecule(item: Molecule) {
 
   }
 
